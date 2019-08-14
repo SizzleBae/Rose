7b113ffd257a4885bc03365bba27286b4873bcc4
Edward Langstrand <eragongreat@outlook.com>
2019-08-14 21:06:32 +0200

    Fireplace beginnings

---
+++ Source/Rose/Tile/Actor/Fireplace.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Tile/TileActor.h"
+#include "Fireplace.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ROSE_API AFireplace : public ATileActor
+{
+	GENERATED_BODY()
+	
+};
